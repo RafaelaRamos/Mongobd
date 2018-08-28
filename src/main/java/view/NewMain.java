cfeda6cae54f72ce0afd1cdcb4acd384de910c56
rafaela <rafaellapereyra@gmail.com>
2018-08-27 22:13:04 -0300

    verificando

--- src/main/java/view/NewMain.java
+++ src/main/java/view/NewMain.java
@@ -25,23 +25,21 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-     VendaDao dao = new VendaDao();
-     Produto produto1 = new Produto(1,"tv",30);
-     Produto produto2 = new Produto(2,"fone de ouvido",30);
-     Produto produto3 = new Produto(4,"lapis",2);
-     ItemVenda itemVenda = new ItemVenda(produto1,3);
-     ItemVenda itemVenda2 = new ItemVenda(produto2,4);
-     List <ItemVenda> itens = new ArrayList<>();
-     //itens.add(itemVenda);
-    // itens.add(itemVenda2);
-     Venda venda = new Venda(3,itens,LocalDateTime.now());
-     ItemVenda itemVenda4 = new ItemVenda(produto3,1);
-     //dao.salvar(venda);
-    //dao.AdicionarItem(3, itemVenda);
-      System.out.println(dao.buscarPorCodigo(3) );
-      
-     
+
+        VendaDao dao = new VendaDao();
+        Produto produto1 = new Produto(1, "teclado", 30);
+        Produto produto2 = new Produto(2, "fone de ouvido", 30);
+        ItemVenda itemVenda = new ItemVenda(produto1, 3);
+        ItemVenda itemVenda2 = new ItemVenda(produto2, 4);
+        List<ItemVenda> itens = new ArrayList<>();
+        itens.add(itemVenda);
+        itens.add(itemVenda2);
+        Venda venda = new Venda(1, itens, LocalDateTime.now());
+        //dao.salvar(venda);
+        //dao.AdicionarItem(1, itemVenda);
+       
+       System.out.println(dao.buscarPorCodigo(1) );
+
     }
-    
+
 }
